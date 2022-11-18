707400a2391ea72717fef60baee06c16f09d8482
Pepeu <pedrosteamgtav@gmail.com>
2022-11-18 16:07:38 -0300

    qr code adapted with link insert by user

---
+++ QRCodeMaker/qr.py
@@ -0,0 +1,13 @@
+import qrcode as qc
+
+# Link enter here
+link = input()
+
+# Data to be encoded
+data = link
+
+# Encoding data using what I put in aspas
+img = qc.make(data)
+
+# Saving as an image file
+img.save('MyQRCode1.png')
